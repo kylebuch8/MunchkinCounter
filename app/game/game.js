7704f3d5d4b68480a94bd61926991d537ad40134
Kyle Buchanan <kylebuch8@gmail.com>
2015-06-18 08:16:13 -0400

    resolving game route

--- app/game/game.js
+++ app/game/game.js
@@ -11,25 +11,55 @@
         .controller('GameController', GameController);
 
     config.$inject = ['$routeProvider'];
-    GameController.$inject = ['$routeParams', '$location', '$firebaseArray', 'firebaseLocation'];
+    GameController.$inject = [
+        '$routeParams',
+        '$location',
+        '$firebaseArray',
+        '$firebaseObject',
+        'firebaseLocation',
+        'game'
+    ];
 
     function config($routeProvider) {
         $routeProvider
             .when('/game/:gameId', {
                 templateUrl: '/game/game.html',
                 controller: 'GameController',
-                controllerAs: 'vm'
+                controllerAs: 'vm',
+                resolve: {
+                    game: gamePrep
+                }
             });
     }
 
-    function GameController($routeParams, $location, $firebaseArray, firebaseLocation) {
+    gamePrep.$inject = ['$route', '$firebaseObject', 'firebaseLocation'];
+
+    function gamePrep($route, $firebaseObject, firebaseLocation) {
+        var gameRef = new Firebase(firebaseLocation + '/games/' + $route.current.params.gameId);
+        return $firebaseObject(gameRef);
+        // gameRef.on('value', function (snapshot) {
+        //     if (snapshot.exists()) {
+        //         return $firebaseObject(gameRef);
+        //     }
+        // });
+    }
+
+    function GameController($routeParams, $location, $firebaseArray, $firebaseObject, firebaseLocation, game) {
         var vm = this,
+            gameRef = new Firebase(firebaseLocation + '/games/' + $routeParams.gameId),
             playersRef = new Firebase(firebaseLocation + '/games/' + $routeParams.gameId + '/players');
 
-        console.log(firebaseLocation);
-
         vm.gameId = $routeParams.gameId;
+        vm.game = $firebaseObject(gameRef);
         vm.players = $firebaseArray(playersRef);
+
+        vm.game.$loaded(function (game) {
+            if (!game.$id) {
+                console.log('there was an error');
+            }
+        }, function (error) {
+            console.log(error);
+        });
 
         vm.goToPlayer = function (index) {
             var key = vm.players.$keyAt(index);
