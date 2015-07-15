0c77ccf92cb63d565e45e79a8dc5c7b19d9f07c3
Kyle Buchanan <kylebuch8@gmail.com>
2015-07-14 22:35:22 -0400

    build process and gulpfile updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    del = require('del'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    htmlreplace = require('gulp-html-replace');
 
 gulp.task('server', function () {
     browserSync({
@@ -20,12 +26,36 @@ gulp.task('less', function () {
         .pipe(gulp.dest('./app/styles'));
 });
 
-gulp.task('copy', function () {
-
+gulp.task('copy', ['clean'], function () {
+    return gulp.src(['./app/**/*.html', './app/bower_components/**/*', './app/images/**/*', './app/styles/**/*'], { base: './app' })
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('build-cordova', function () {
 
+});
+
+gulp.task('clean', function (cb) {
+    del(['./dist/**/*'], cb);
+});
+
+gulp.task('replace', ['clean', 'copy'], function () {
+    return gulp.src('./app/index.html')
+        .pipe(htmlreplace({
+            'js': 'app.min.js'
+        }))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['clean', 'copy', 'replace'], function () {
+    return gulp.src(['./app/**/*.js', '!./app/bower_components/**/*'])
+        .pipe(gulp.dest('./dist'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.concat.js'))
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', ['server']);
